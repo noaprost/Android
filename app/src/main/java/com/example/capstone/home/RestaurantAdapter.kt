595135a2ccb8d4fb15ca97cd8f56a4e0001afad1
mummhy0811 <mummhy0811@inu.ac.kr>
2023-02-11 19:30:02 +0900

    feat: 찜한 가게 리스트 생성
    +) 레스토랑 리사이클러뷰 ui 수정

--- app/src/main/java/com/example/capstone/home/RestaurantAdapter.kt
+++ app/src/main/java/com/example/capstone/home/RestaurantAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.capstone.R
@@ -34,10 +35,11 @@ class RestaurantAdapter(val restaurantList: ArrayList<Restaurant>) : RecyclerVie
         holder.rating.text = restaurantList.get(position).rating.toString()
         holder.commentNumber.text = restaurantList.get(position).commentNumber.toString()
         holder.name.text = restaurantList.get(position).name
+        holder.restaurantImg.clipToOutline=true
     }
 
     class ResViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val restaurantImg : ImageButton = itemView.findViewById<ImageButton>(R.id.restaurantImage) // 음식점 메인 이미지
+        val restaurantImg : ImageView = itemView.findViewById<ImageView>(R.id.restaurantImage) // 음식점 메인 이미지
         val rating : TextView = itemView.findViewById<TextView>(R.id.rating)
         val commentNumber: TextView = itemView.findViewById<TextView>(R.id.commentNumber)
         val name: TextView = itemView.findViewById<TextView>(R.id.restaurantName)
