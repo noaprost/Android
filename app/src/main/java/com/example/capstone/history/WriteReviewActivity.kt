16a4454224e4e48e2160741083a6fb46a3cebed2
mummhy0811 <mummhy0811@inu.ac.kr>
2023-02-16 18:40:34 +0900

    fix: 리뷰작성-앨범 연결 및 등록버튼 추가

--- app/src/main/java/com/example/capstone/history/WriteReviewActivity.kt
+++ app/src/main/java/com/example/capstone/history/WriteReviewActivity.kt
@@ -1,14 +1,20 @@
 package com.example.capstone.history
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.capstone.databinding.ActivityWriteReviewBinding
 
 
 class WriteReviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteReviewBinding
+    private lateinit var reviewImage: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +28,28 @@ class WriteReviewActivity : AppCompatActivity() {
                 binding.writeReviewLength.setText(s.length.toString() + "/400")
             }
         })
-
+        binding.writeReviewImage.setOnClickListener {
+            val intent=Intent(Intent.ACTION_PICK)
+            intent.type="image/*"
+            activityResult.launch(intent)
+        }
+        binding.writeReviewButton.setOnClickListener{
+            //todo 리뷰 등록코드 작성
+        }
         binding.backButton.setOnClickListener {
             finish()
         }
     }
+
+    private val activityResult:ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode== RESULT_OK && it.data != null){
+                val uri = it.data!!.data
+                uri!!.also { reviewImage = it }
+                Glide.with(this)
+                    .load(uri) //이미지
+                    .into(binding.writeReviewImage)
+            }
+
+        }
 }
