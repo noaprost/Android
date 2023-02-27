22539e0e21bb8df9660a49085e09c2163213c1eb
mummhy0811 <mummhy0811@inu.ac.kr>
2023-02-27 19:24:39 +0900

    feat: 팝업창 연결

--- app/src/main/java/com/example/capstone/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/capstone/mypage/MyPageFragment.kt
@@ -1,18 +1,23 @@
 package com.example.capstone.mypage
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentManager
+import com.example.capstone.ConfirmDialogInterface
+import com.example.capstone.CustomDialog
 import com.example.capstone.MainActivity
 import com.example.capstone.R
 import com.example.capstone.databinding.FragmentMyPageBinding
 import com.example.capstone.history.WriteReviewActivity
 
 
-class MyPageFragment : Fragment() {
+class MyPageFragment : Fragment(), ConfirmDialogInterface {
     private var _binding: FragmentMyPageBinding? = null
     private val binding get() = _binding!!
 
@@ -39,8 +44,19 @@ class MyPageFragment : Fragment() {
             val intent = Intent(activity, WriteReviewActivity::class.java)
             startActivity(intent)
         }
+        binding.myLogout.setOnClickListener {
+            val dialog = CustomDialog(this, "로그아웃 하시겠습니까?", 0, 0)
+            dialog.isCancelable = false
+            this@MyPageFragment.fragmentManager?.let { it1 -> dialog.show(it1, "ConfirmDialog") }
+        }
         return root
 
+    }
+
+    override fun onYesButtonClick(num: Int, theme: Int) {
+        when(num){
+            //todo 로그아웃 연결
+        }
     }
 
 }
