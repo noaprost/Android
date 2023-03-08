ee3a29d4616198d3317504e9179c40583595fc07
noaprost <kangjiwon1850@gmail.com>
2023-03-09 01:16:36 +0900

    feat: 이전 대기내역 recyclerView 연결

--- app/src/main/java/com/example/capstone/GPS/GPSActivity.kt
+++ app/src/main/java/com/example/capstone/GPS/GPSActivity.kt
@@ -2,10 +2,6 @@ package com.example.capstone.GPS
 
 import androidx.appcompat.app.AppCompatActivity
 
-// const val TAG = GPSActivity.class.getSimpleName()
-const val GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE = 101
-const val GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE = 100
-
 class GPSActivity : AppCompatActivity(){
 
 
