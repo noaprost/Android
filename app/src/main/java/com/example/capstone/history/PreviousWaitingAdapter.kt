9856d9c5eb1ae6662b100be8d87eea5315f05e0e
noaprost <kangjiwon1850@gmail.com>
2023-03-09 01:20:39 +0900

    feat: 이전 대기내역 recyclerView 연결

---
+++ app/src/main/java/com/example/capstone/history/PreviousWaitingAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.capstone.history
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.capstone.R
+
+
+class PreviousWaitingAdapter(val previousWaitingList: ArrayList<PreviousWaiting>) : RecyclerView.Adapter<PreviousWaitingAdapter.WaitingViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ) : PreviousWaitingAdapter.WaitingViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_restaurant, parent, false)
+        return WaitingViewHolder(view).apply {
+            itemView.setOnClickListener {
+                val curPos : Int = adapterPosition
+                val watingHistory : PreviousWaiting = previousWaitingList.get(curPos)
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return previousWaitingList.size
+
+    }
+
+    override fun onBindViewHolder(holder: WaitingViewHolder, position: Int) {
+        holder.txtWaitingDate.text = previousWaitingList.get(position).txtWaitingDate
+        holder.waitingImg.setImageResource(previousWaitingList.get(position).waitingImg)
+        holder.waitingTitle.text = previousWaitingList.get(position).waitingTitle
+        holder.waitingImg.clipToOutline=true
+    }
+
+    class WaitingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtWaitingDate : TextView = itemView.findViewById(R.id.txtWaitingDate)
+        val waitingImg: ImageView = itemView.findViewById(R.id.waitingImg)
+        val waitingTitle: TextView = itemView.findViewById(R.id.txtWatingTitle)
+    }
+}
