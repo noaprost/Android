e429093cef71323a8da0cf72a9b01e3992a50144
mummhy0811 <mummhy0811@inu.ac.kr>
2023-03-19 21:43:52 +0900

    feat: 메인화면 위치 인증

--- app/src/main/java/com/example/capstone/list/ViewPageAdapter.kt
+++ app/src/main/java/com/example/capstone/list/ViewPageAdapter.kt
@@ -2,9 +2,7 @@ package com.example.capstone.list
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.capstone.map.MapFragment
-import com.example.capstone.mypage.MyPageFragment
-import com.example.capstone.restaurant.*
+
 
 class ViewPagerAdapter (fragment : ListFragment) : FragmentStateAdapter(fragment){
     override fun getItemCount(): Int = 10
