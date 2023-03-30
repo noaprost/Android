52692413472012749158c8dda8348ec707c8d099
mummhy0811 <mummhy0811@inu.ac.kr>
2023-03-30 14:56:54 +0900

    feat: 레트로핏 연결

--- app/src/main/java/com/example/capstone/login/LoginActivity.kt
+++ app/src/main/java/com/example/capstone/login/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.capstone.login
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.capstone.databinding.ActivityLoginBinding
-import com.example.capstone.databinding.ActivityMyReviewBinding
 
 
 class LoginActivity : AppCompatActivity() {
--- app/src/main/java/com/example/capstone/retrofit/Constants.kt
+++ app/src/main/java/com/example/capstone/retrofit/Constants.kt
@@ -2,6 +2,6 @@ package com.example.capstone.retrofit
 
 
 object API{
-    const val BASE_URL : String = "http://54.209.17.39:8080"
+    const val BASE_URL : String = "http://ec2-13-125-237-193.ap-northeast-2.compute.amazonaws.com"
     const val CLIENT_ID : String = ""
 }
