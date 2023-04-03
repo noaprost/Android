cb24f4418e86aa85e3a9574e8ed2335980055fc0
noaprost <kangjiwon1850@gmail.com>
2023-04-03 15:58:48 +0900

    feat: history, search recyclerView refactoring

--- app/src/main/java/com/example/capstone/history/HistoryFragment.kt
+++ app/src/main/java/com/example/capstone/history/HistoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.capstone.R
 import com.example.capstone.databinding.FragmentHistoryBinding
@@ -14,6 +15,7 @@ class HistoryFragment : Fragment() {
 
     private var _binding : FragmentHistoryBinding? = null
     private val binding get() = _binding!!
+    private var previousWaitingList = ArrayList<PreviousWaiting>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,25 +25,79 @@ class HistoryFragment : Fragment() {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val previousWaitingList = arrayListOf(
-            PreviousWaiting("2022-12-09", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2022-12-12", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2022-12-13", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2022-12-22", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2022-12-25", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2022-12-31", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2023-01-08", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2023-01-22", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-            PreviousWaiting("2023-02-14", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점"),
-        )
+
+        previousWaitingList.apply {
+            add(
+                PreviousWaiting("2022-12-09", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+            )
+
+            add(
+                PreviousWaiting("2022-12-12", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+
+            add(
+                PreviousWaiting("2022-12-13", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+
+            add(
+                PreviousWaiting("2022-12-22", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+
+            add(
+                PreviousWaiting("2022-12-25", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+
+                )
+
+            add(
+                PreviousWaiting("2022-12-31", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+
+                )
+
+            add(
+                PreviousWaiting("2023-01-08", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+
+            add(
+                PreviousWaiting("2023-01-22", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+
+            add(
+                PreviousWaiting("2023-02-14", R.drawable.dummy_restaurant_image, "온리원 파스타 송도점")
+                )
+        }
 
         binding.previousWaitingRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         binding.previousWaitingRecyclerView.setHasFixedSize(true)
-        binding.previousWaitingRecyclerView.adapter = PreviousWaitingAdapter(previousWaitingList, this)
-
-
-
+        binding.previousWaitingRecyclerView.adapter = PreviousWaitingAdapter(previousWaitingList)
         return root
+    }
+
+    inner class PreViewHolder(view: View): RecyclerView.ViewHolder(view){
+        private lateinit var prewait: PreviousWaiting
+
+        fun bind(prewait: PreviousWaiting){
+            this.prewait = prewait
+
+            itemView.setOnClickListener {
+
+            }
+        }
+    }
+
+    inner class PreviousWaitingAdapter(private val preList:List<PreviousWaiting>): RecyclerView.Adapter<PreViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PreViewHolder {
+            val view = layoutInflater.inflate(R.layout.item_previous_waiting_info, parent, false)
+            return PreViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return previousWaitingList.size
+        }
+
+        override fun onBindViewHolder(holder: PreViewHolder, position: Int) {
+            val post=preList[position]
+            holder.bind(post)
+        }
     }
 
 }
