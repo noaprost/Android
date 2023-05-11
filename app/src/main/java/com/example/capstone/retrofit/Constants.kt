4712b428c2cf3cc9797864c4795ba555f795b24e
mummhy0811 <mummhy0811@inu.ac.kr>
2023-05-11 16:45:46 +0900

    fix: 나의 리뷰 api 수정

--- app/src/main/java/com/example/capstone/retrofit/Constants.kt
+++ app/src/main/java/com/example/capstone/retrofit/Constants.kt
@@ -2,6 +2,6 @@ package com.example.capstone.retrofit
 
 
 object API{
-    const val BASE_URL : String = "http://ec2-13-125-237-193.ap-northeast-2.compute.amazonaws.com:3000"
+    const val BASE_URL : String = "http://ec2-13-125-237-193.ap-northeast-2.compute.amazonaws.com:3000"//"http://ec2-3-35-238-37.ap-northeast-2.compute.amazonaws.com:3000"//
     const val CLIENT_ID : String = ""
 }
